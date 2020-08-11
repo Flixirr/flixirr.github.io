87148893aa6bd25ab5425a8898018d064018c8f1
Flixirr <filip.0krezel@gmail.com>
2020-08-11 18:12:49 +0200

    Connected with FB Api

---
+++ js/fbapi.js
@@ -0,0 +1,24 @@
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '309464026839831',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v8.0'
+    });
+    
+    FB.login(function(response) {
+
+    });
+
+    FB.AppEvents.logPageView();   
+      
+  };
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
